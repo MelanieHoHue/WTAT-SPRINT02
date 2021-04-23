aa713116e237170d617ef371b7e7a3a70acf06b0
M.H-H <Melanie.Holzapfel-Huettenrauch@Student.HTW-Berlin.de>
2021-04-23 08:40:05 +0200

    added general logging for requests

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const port = 3000,
 express = require("express"),
 app = express();
 
+app.use((req, res ,next) => {
+    console.log(`Request was made for ${req.url}`);
+    next();
+});
+
 app.get("/items/:vegetable", (req, res) => {
     let veg = req.params.vegetable;
     res.send(`This is th epage for ${veg}`);
